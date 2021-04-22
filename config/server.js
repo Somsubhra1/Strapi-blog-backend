0a631ba0d2d32e5f0f14a4566a8daeef7559bba9
Somsubhra Das <das.somsubhra1@gmail.com>
2021-04-23 02:57:30 +0530

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8f91206309d2ac472e2e5196e8146ee7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64ce7e90-798d-47a0-a4cf-3751662d396e'
+};
